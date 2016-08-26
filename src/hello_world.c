86094967f048f41e07da5d0b9d0a3759e31bc142
MrFoxhead <MrFoxhead@users.noreply.github.com>
2016-08-26 18:27:06 +0200

    Finished!

--- src/hello_world.c
+++ src/hello_world.c
@@ -6,13 +6,14 @@ static TextLayer *date_layer;
 static GBitmap *s_bitmap;
 static BitmapLayer *s_bitmap_layer;
 
-
 void handle_timechanges(struct tm *tick_time, TimeUnits units_changed){
 	static char time_buffer[10];
 	static char date_buffer[10];
 	strftime(time_buffer, sizeof(time_buffer), "%H:%M", tick_time);
+	text_layer_set_background_color(s_text_layer, GColorClear);
 	text_layer_set_text(s_text_layer, time_buffer);
 	
+	text_layer_set_background_color(date_layer, GColorClear);
 	strftime(date_buffer, sizeof(date_buffer), "%b %e", tick_time);
 	text_layer_set_text(date_layer, date_buffer);
 }
@@ -39,13 +40,16 @@ static void init(void) {
 	s_text_layer = text_layer_create(bounds);
   
   // Set the font and text alignment
-	GFont Chicago = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_Font_Chicago_34));
-	s_text_layer = text_layer_create(GRect(3, 61, 121, 41));
+	GFont Chicago = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_Font_Chicago_36));
+	
+	s_text_layer = text_layer_create(GRect(3, 60, 121, 41));
 	text_layer_set_font(s_text_layer, Chicago);
 	text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
 	
-	date_layer = text_layer_create(GRect(61, 46, 78, 13));
-	text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
+	GFont goth = fonts_get_system_font(FONT_KEY_GOTHIC_14);
+	date_layer = text_layer_create(GRect(61, 43, 68, 16));
+	text_layer_set_font(date_layer, goth);
+	text_layer_set_text_alignment(date_layer, GTextAlignmentRight);
 	
 	tick_timer_service_subscribe(MINUTE_UNIT, handle_timechanges);
 
